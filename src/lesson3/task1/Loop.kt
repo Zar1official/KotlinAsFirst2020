01e1a7ee3cb0dac1b7819f17cc780a16ead09a36
Zar <dimzar233445@gmail.com>
2021-09-10 16:02:04 +0300

    found decision for fib recursion

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -81,9 +81,14 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = when (n) {
-    1, 2 -> 1
-    else -> fib(n - 1) + fib(n - 2)
+val cache = mutableMapOf(1 to 1, 2 to 1)
+fun fib(n: Int): Int {
+    if (cache.containsKey(n))
+        return cache[n]!!
+    else
+        cache.put(n, fib(n - 1) + fib(n - 2))
+    return cache[n]!!
+
 }
 
 /**
